2d7ce24bb049803048fb70bb098369b6e29e48ff
gridik26 <gridi26@hotmail.com>
2021-04-11 22:44:13 +0200

    creation projet

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../App.css';
+import './banner';
+import Cart from './cart';
+import Banner from './banner';
+
+function App() {
+  return (
+    <div>
+      <Banner/>
+      <Cart/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/cart.js
@@ -0,0 +1,19 @@
+function Cart() {
+    const prixMonestera = 8;
+    const prixLierre = 10;
+    const prixBouquet = 15;
+    return (
+        <div>
+            <h2>Panier</h2>
+            <ul>
+                <li>Monestera : { prixMonestera }€</li>
+                <li>Lierre : {prixLierre}€</li>
+                <li>Bouquet : {prixBouquet}€</li>
+            </ul>
+            <p>Total du panier { prixMonestera + prixLierre + prixBouquet }€ </p>
+        </div>
+        
+    );
+}
+
+export default Cart
